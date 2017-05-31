9e1eb8d5b4138b817086e44250dde489a76c0420
emlody <emlody@gmail.com>
2017-05-31 23:10:39 +0200

    wygranaGra();

--- src/javaZalProject/JavaZalProject.java
+++ src/javaZalProject/JavaZalProject.java
@@ -22,12 +22,10 @@ public class JavaZalProject {
 	private JFrame frame;
 	
 	private String startGry = "X";
-	private int licznikX = 0;
-	private int licznikO = 0;
-
-	private JLabel lblWynikO;
-
-	private JLabel lblWynikX;
+	
+	private int licznikX = 0, licznikO = 0;
+	private JLabel lblWynikO , lblWynikX;
+	private JLabel btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9;
 	
 
 	/**
@@ -52,13 +50,13 @@ public class JavaZalProject {
 	public JavaZalProject() 
 	{
 		initialize();
-		wynikGry();
+		//wynikGry();
 	}
 	
 	private void wynikGry()
 	{
-		lblWynikO.setText(String.valueOf(licznikO));
 		lblWynikX.setText(String.valueOf(licznikX));
+		lblWynikO.setText(String.valueOf(licznikO));
 	}
 
 	private void wybierzGracza()
@@ -71,6 +69,19 @@ public class JavaZalProject {
 		{
 			startGry="X";
 		}
+	}
+	
+	private void wygranaGra()
+	{
+		String b1 = btn1.getText();
+		String b2 = btn2.getText();
+		String b3 = btn3.getText();
+		String b4 = btn4.getText();
+		String b5 = btn5.getText();
+		String b6 = btn6.getText();
+		String b7 = btn7.getText();
+		String b8 = btn8.getText();
+		String b9 = btn9.getText();
 	}
 	
 	/**
@@ -117,6 +128,20 @@ public class JavaZalProject {
 		panel_2.setLayout(new BorderLayout(0, 0));
 		
 		JButton btn2 = new JButton("");
+		btn2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				btn2.setText(startGry);
+				if(startGry.equalsIgnoreCase("X"))
+				{
+					btn2.setForeground(Color.RED);
+				}
+				else
+				{
+					btn2.setForeground(Color.BLUE);
+				}
+				wybierzGracza();
+			}
+		});
 		btn2.setFont(new Font("Tahoma", Font.BOLD, 99));
 		panel_2.add(btn2, BorderLayout.CENTER);
 		
@@ -126,7 +151,21 @@ public class JavaZalProject {
 		panel_3.setLayout(new BorderLayout(0, 0));
 		
 		JButton btn3 = new JButton("");
-		btn3.setFont(new Font("Tahoma", Font.BOLD, 50));
+		btn3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btn3.setText(startGry);
+				if(startGry.equalsIgnoreCase("X"))
+				{
+					btn3.setForeground(Color.RED);
+				}
+				else
+				{
+					btn3.setForeground(Color.BLUE);
+				}
+				wybierzGracza();
+			}
+		});
+		btn3.setFont(new Font("Tahoma", Font.BOLD, 99));
 		panel_3.add(btn3, BorderLayout.CENTER);
 		
 		JPanel panel_4 = new JPanel();
@@ -135,7 +174,21 @@ public class JavaZalProject {
 		panel_4.setLayout(new BorderLayout(0, 0));
 		
 		JButton btn4 = new JButton("");
-		btn4.setFont(new Font("Tahoma", Font.BOLD, 50));
+		btn4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btn4.setText(startGry);
+				if(startGry.equalsIgnoreCase("X"))
+				{
+					btn4.setForeground(Color.RED);
+				}
+				else
+				{
+					btn4.setForeground(Color.BLUE);
+				}
+				wybierzGracza();
+			}
+		});
+		btn4.setFont(new Font("Tahoma", Font.BOLD, 99));
 		panel_4.add(btn4, BorderLayout.CENTER);
 		
 		JPanel panel_5 = new JPanel();
@@ -144,7 +197,21 @@ public class JavaZalProject {
 		panel_5.setLayout(new BorderLayout(0, 0));
 		
 		JButton btn5 = new JButton("");
-		btn5.setFont(new Font("Tahoma", Font.BOLD, 50));
+		btn5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btn5.setText(startGry);
+				if(startGry.equalsIgnoreCase("X"))
+				{
+					btn5.setForeground(Color.RED);
+				}
+				else
+				{
+					btn5.setForeground(Color.BLUE);
+				}
+				wybierzGracza();
+			}
+		});
+		btn5.setFont(new Font("Tahoma", Font.BOLD, 99));
 		panel_5.add(btn5, BorderLayout.CENTER);
 		
 		JPanel panel_6 = new JPanel();
@@ -153,7 +220,21 @@ public class JavaZalProject {
 		panel_6.setLayout(new BorderLayout(0, 0));
 		
 		JButton btn6 = new JButton("");
-		btn6.setFont(new Font("Tahoma", Font.BOLD, 50));
+		btn6.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btn6.setText(startGry);
+				if(startGry.equalsIgnoreCase("X"))
+				{
+					btn6.setForeground(Color.RED);
+				}
+				else
+				{
+					btn6.setForeground(Color.BLUE);
+				}
+				wybierzGracza();
+			}
+		});
+		btn6.setFont(new Font("Tahoma", Font.BOLD, 99));
 		panel_6.add(btn6, BorderLayout.CENTER);
 		
 		JPanel panel_7 = new JPanel();
@@ -162,7 +243,21 @@ public class JavaZalProject {
 		panel_7.setLayout(new BorderLayout(0, 0));
 		
 		JButton btn7 = new JButton("");
-		btn7.setFont(new Font("Tahoma", Font.BOLD, 50));
+		btn7.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btn7.setText(startGry);
+				if(startGry.equalsIgnoreCase("X"))
+				{
+					btn7.setForeground(Color.RED);
+				}
+				else
+				{
+					btn7.setForeground(Color.BLUE);
+				}
+				wybierzGracza();
+			}
+		});
+		btn7.setFont(new Font("Tahoma", Font.BOLD, 99));
 		panel_7.add(btn7, BorderLayout.CENTER);
 		
 		JPanel panel_8 = new JPanel();
@@ -171,7 +266,21 @@ public class JavaZalProject {
 		panel_8.setLayout(new BorderLayout(0, 0));
 		
 		JButton btn8 = new JButton("");
-		btn8.setFont(new Font("Tahoma", Font.BOLD, 50));
+		btn8.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btn8.setText(startGry);
+				if(startGry.equalsIgnoreCase("X"))
+				{
+					btn8.setForeground(Color.RED);
+				}
+				else
+				{
+					btn8.setForeground(Color.BLUE);
+				}
+				wybierzGracza();
+			}
+		});
+		btn8.setFont(new Font("Tahoma", Font.BOLD, 99));
 		panel_8.add(btn8, BorderLayout.CENTER);
 		
 		JPanel panel_9 = new JPanel();
@@ -180,7 +289,21 @@ public class JavaZalProject {
 		panel_9.setLayout(new BorderLayout(0, 0));
 		
 		JButton btn9 = new JButton("");
-		btn9.setFont(new Font("Tahoma", Font.BOLD, 50));
+		btn9.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btn9.setText(startGry);
+				if(startGry.equalsIgnoreCase("X"))
+				{
+					btn9.setForeground(Color.RED);
+				}
+				else
+				{
+					btn9.setForeground(Color.BLUE);
+				}
+				wybierzGracza();
+			}
+		});
+		btn9.setFont(new Font("Tahoma", Font.BOLD, 99));
 		panel_9.add(btn9, BorderLayout.CENTER);
 		
 		JPanel panel_10 = new JPanel();
