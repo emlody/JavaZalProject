ab324ba34fc2bfadc3cc421068018209b1686451
emlody <emlody@gmail.com>
2017-05-31 11:21:37 +0200

    panele

--- src/javaZalProject/JavaZalProject.java
+++ src/javaZalProject/JavaZalProject.java
@@ -54,49 +54,79 @@ public class JavaZalProject {
 		panel.setLayout(new GridLayout(5, 3, 2, 2));
 		
 		JPanel panel_1 = new JPanel();
+		panel_1.setBorder(new LineBorder(new Color(0, 0, 0), 2, true));
 		panel.add(panel_1);
+		panel_1.setLayout(new BorderLayout(0, 0));
 		
 		JPanel panel_2 = new JPanel();
+		panel_2.setBorder(new LineBorder(new Color(0, 0, 0), 2, true));
 		panel.add(panel_2);
+		panel_2.setLayout(new BorderLayout(0, 0));
 		
 		JPanel panel_3 = new JPanel();
+		panel_3.setBorder(new LineBorder(new Color(0, 0, 0), 2, true));
 		panel.add(panel_3);
+		panel_3.setLayout(new BorderLayout(0, 0));
 		
 		JPanel panel_5 = new JPanel();
+		panel_5.setBorder(new LineBorder(new Color(0, 0, 0), 2, true));
 		panel.add(panel_5);
+		panel_5.setLayout(new BorderLayout(0, 0));
 		
 		JPanel panel_4 = new JPanel();
+		panel_4.setBorder(new LineBorder(new Color(0, 0, 0), 2, true));
 		panel.add(panel_4);
+		panel_4.setLayout(new BorderLayout(0, 0));
 		
 		JPanel panel_6 = new JPanel();
+		panel_6.setBorder(new LineBorder(new Color(0, 0, 0), 2, true));
 		panel.add(panel_6);
+		panel_6.setLayout(new BorderLayout(0, 0));
 		
 		JPanel panel_7 = new JPanel();
+		panel_7.setBorder(new LineBorder(new Color(0, 0, 0), 2, true));
 		panel.add(panel_7);
+		panel_7.setLayout(new BorderLayout(0, 0));
 		
 		JPanel panel_10 = new JPanel();
+		panel_10.setBorder(new LineBorder(new Color(0, 0, 0), 2, true));
 		panel.add(panel_10);
+		panel_10.setLayout(new BorderLayout(0, 0));
 		
 		JPanel panel_9 = new JPanel();
+		panel_9.setBorder(new LineBorder(new Color(0, 0, 0), 2, true));
 		panel.add(panel_9);
+		panel_9.setLayout(new BorderLayout(0, 0));
 		
 		JPanel panel_8 = new JPanel();
+		panel_8.setBorder(new LineBorder(new Color(0, 0, 0), 2, true));
 		panel.add(panel_8);
+		panel_8.setLayout(new BorderLayout(0, 0));
 		
 		JPanel panel_11 = new JPanel();
+		panel_11.setBorder(new LineBorder(new Color(0, 0, 0), 2, true));
 		panel.add(panel_11);
+		panel_11.setLayout(new BorderLayout(0, 0));
 		
 		JPanel panel_12 = new JPanel();
+		panel_12.setBorder(new LineBorder(new Color(0, 0, 0), 2, true));
 		panel.add(panel_12);
+		panel_12.setLayout(new BorderLayout(0, 0));
 		
 		JPanel panel_13 = new JPanel();
+		panel_13.setBorder(new LineBorder(new Color(0, 0, 0), 2, true));
 		panel.add(panel_13);
+		panel_13.setLayout(new BorderLayout(0, 0));
 		
 		JPanel panel_14 = new JPanel();
+		panel_14.setBorder(new LineBorder(new Color(0, 0, 0), 2, true));
 		panel.add(panel_14);
+		panel_14.setLayout(new BorderLayout(0, 0));
 		
 		JPanel panel_15 = new JPanel();
+		panel_15.setBorder(new LineBorder(new Color(0, 0, 0), 2, true));
 		panel.add(panel_15);
+		panel_15.setLayout(new BorderLayout(0, 0));
 	}
 
 }
