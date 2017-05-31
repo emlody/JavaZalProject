48a80cdd72eb146da420005caa3c0bcf59aa1d1f
emlody <emlody@gmail.com>
2017-05-31 10:17:04 +0200

    panele

--- src/javaZalProject/JavaZalProject.java
+++ src/javaZalProject/JavaZalProject.java
@@ -53,50 +53,50 @@ public class JavaZalProject {
 		frame.getContentPane().add(panel, BorderLayout.CENTER);
 		panel.setLayout(new GridLayout(5, 3, 2, 2));
 		
-		JLabel lblNewLabel = new JLabel("New label");
-		panel.add(lblNewLabel);
+		JPanel panel_1 = new JPanel();
+		panel.add(panel_1);
 		
-		JLabel lblNewLabel_1 = new JLabel("New label");
-		panel.add(lblNewLabel_1);
+		JPanel panel_2 = new JPanel();
+		panel.add(panel_2);
 		
-		JLabel lblNewLabel_2 = new JLabel("New label");
-		panel.add(lblNewLabel_2);
+		JPanel panel_3 = new JPanel();
+		panel.add(panel_3);
 		
-		JLabel lblNewLabel_3 = new JLabel("New label");
-		panel.add(lblNewLabel_3);
+		JPanel panel_5 = new JPanel();
+		panel.add(panel_5);
 		
-		JLabel lblNewLabel_4 = new JLabel("New label");
-		panel.add(lblNewLabel_4);
+		JPanel panel_4 = new JPanel();
+		panel.add(panel_4);
 		
-		JLabel lblNewLabel_5 = new JLabel("New label");
-		panel.add(lblNewLabel_5);
+		JPanel panel_6 = new JPanel();
+		panel.add(panel_6);
 		
-		JLabel lblNewLabel_6 = new JLabel("New label");
-		panel.add(lblNewLabel_6);
+		JPanel panel_7 = new JPanel();
+		panel.add(panel_7);
 		
-		JLabel lblNewLabel_7 = new JLabel("New label");
-		panel.add(lblNewLabel_7);
+		JPanel panel_10 = new JPanel();
+		panel.add(panel_10);
 		
-		JLabel lblNewLabel_8 = new JLabel("New label");
-		panel.add(lblNewLabel_8);
+		JPanel panel_9 = new JPanel();
+		panel.add(panel_9);
 		
-		JLabel lblNewLabel_9 = new JLabel("New label");
-		panel.add(lblNewLabel_9);
+		JPanel panel_8 = new JPanel();
+		panel.add(panel_8);
 		
-		JLabel lblNewLabel_10 = new JLabel("New label");
-		panel.add(lblNewLabel_10);
+		JPanel panel_11 = new JPanel();
+		panel.add(panel_11);
 		
-		JLabel lblNewLabel_11 = new JLabel("New label");
-		panel.add(lblNewLabel_11);
+		JPanel panel_12 = new JPanel();
+		panel.add(panel_12);
 		
-		JLabel lblNewLabel_12 = new JLabel("New label");
-		panel.add(lblNewLabel_12);
+		JPanel panel_13 = new JPanel();
+		panel.add(panel_13);
 		
-		JLabel lblNewLabel_13 = new JLabel("New label");
-		panel.add(lblNewLabel_13);
+		JPanel panel_14 = new JPanel();
+		panel.add(panel_14);
 		
-		JLabel lblNewLabel_14 = new JLabel("New label");
-		panel.add(lblNewLabel_14);
+		JPanel panel_15 = new JPanel();
+		panel.add(panel_15);
 	}
 
 }
